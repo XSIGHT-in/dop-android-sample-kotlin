1725af51ee65d111de2d28955c2b345e845d0e43
justin CHOI <justin.yg.choi@gmail.com>
2020-05-11 18:09:01 +0900

    IMPLEMENT: Create CollectionActivity

---
+++ app/src/main/java/in/xsight/sdk/android/sample/kotlin/CollectionActivity.kt
@@ -0,0 +1,12 @@
+package `in`.xsight.sdk.android.sample.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CollectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collection)
+    }
+}
